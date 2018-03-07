31d26224cfbfa574efab747f4f941d5fcf1e3171
Luke Zappia <lazappi@users.noreply.github.com>
2018-03-07 15:16:42 +1100

    Cache titles

--- process_csv.R
+++ process_csv.R
@@ -96,8 +96,64 @@ get_pkgs <- function() {
 #'
 #' @return data.frame containing categories and descriptions
 get_descriptions <- function() {
-    message("Adding category descriptions...")
+    message("Getting category descriptions...")
     descs <- read_json("docs/data/descriptions.json", simplifyVector = TRUE)
+}
+
+
+#' Get cached reference titles
+#'
+#' Read `docs/data/titles.csv`, create if missing
+#'
+#' @return title containing DOIs and titles
+get_cached_titles <- function() {
+    message("Getting cached titles...")
+
+    if (!file.exists("docs/data/titles.csv")) {
+        message("Cache file missing, creating...")
+        write_lines("DOI,Title", "docs/data/titles.csv")
+    }
+
+    titles <- read_csv("docs/data/titles.csv",
+                       col_types = cols(
+                           DOI = col_character(),
+                           Title = col_character()
+                       )
+              )
+}
+
+
+#' Add to titles cache
+#'
+#' Add a DOI-Title pair to the title cache
+#'
+#' @param swsheet Tibble containing software table
+#' @param titles_cache Current titles cache
+#'
+#' @return Updated titles cache
+add_to_titles_cache <- function(swsheet, titles_cache) {
+    message("Adding new titles to cache...")
+
+    n_added <- 0
+    for (dois in swsheet$DOIs) {
+        for (doi in str_split(dois, ";")[[1]]) {
+            if (!is.na(doi) & !(doi %in% titles_cache$DOI)) {
+                title <- cr_works(doi)$data$title
+
+                if (!is.null(title)) {
+                    titles_cache <- bind_rows(titles_cache,
+                                              c(DOI = doi, Title = title))
+                    message(doi, " added to cache")
+                    n_added <- n_added + 1
+                }
+            }
+        }
+    }
+
+    write_csv(titles_cache, "docs/data/titles.csv")
+    message("Added ", n_added, " new titles to cache")
+
+    return(titles_cache)
 }
 
 
@@ -122,14 +178,41 @@ fix_doi <- function(swsheet) {
 }
 
 
+#' Get titles
+#'
+#' Get title for DOIs. Return from cache if present, otherwise requests from
+#' Crossref
+#'
+#' @param dois Character vector of dois
+#' @param titles_cache Tibble containing cached titles
+#'
+#' @return vector of titles
+get_titles <- function(dois, titles_cache) {
+
+    titles <- map(dois, function(doi) {
+        if (doi %in% titles_cache$DOI) {
+            titles_cache %>%
+                filter(DOI == doi) %>%
+                pull(Title)
+        } else {
+            NA
+        }
+    }) %>%
+        flatten_chr()
+
+    return(titles)
+}
+
+
 #' Add references
 #'
 #' Covert references to list column and get citations
 #'
 #' @param swsheet Tibble containing software table
+#' @param titles_cache Tibble containing titles cache
 #'
-#' @return swsheet with additional column
-add_refs <- function(swsheet) {
+#' @return swsheet with additional columns
+add_refs <- function(swsheet, titles_cache) {
 
     message("Adding references...")
 
@@ -164,11 +247,7 @@ add_refs <- function(swsheet) {
             return(cite)
         })
 
-        titles <- cr_works(dois)$data$title
-
-        print(x)
-        print(dois)
-        print(titles)
+        titles <- get_titles(dois, titles_cache)
 
         ref <- tibble(Title = titles,
                       DOI = dois,
@@ -615,11 +694,15 @@ process_csv <- function() {
     swsheet <- get_swsheet()
     pkgs <- get_pkgs()
     descs <- get_descriptions()
+    titles_cache <- get_cached_titles()
+
+    # Add new titles
+    titles_cache <- add_to_titles_cache(swsheet, titles_cache)
 
     # Process table
     message("Processing table...")
     swsheet <- swsheet %>%
-        add_refs() %>%
+        add_refs(titles_cache) %>%
         add_github() %>%
         add_repos(pkgs) #%>%
         #add_citations()
