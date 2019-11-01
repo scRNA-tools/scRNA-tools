dc38a9ad7fe7b8b46757928611e8ab984273d516
Luke Zappia <lazappi@users.noreply.github.com>
2019-11-01 16:57:24 +0100

    Add setup script

---
+++ setup.sh
@@ -0,0 +1,6 @@
+echo "Bootstrapping packrat..."
+R -e "options(repos='https://cloud.r-project.org')" --args --bootstrap-packrat
+echo "Restoring packrat..."
+R -q -e "packrat::restore(overwrite.dirty = TRUE, prompt = FALSE, restart = FALSE)"
+echo "Setting upstream remote..."
+git remote add upstream git@github.com:Oshlack/scRNA-tools.git
